44c05a1bedc07aa8841f1a35f12f304473c456b7
Michael B <michaelbarr24@hotmail.com>
2022-06-09 19:54:25 +0200

    Add server setup

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from "express";
 const app = express();
 import logger from "./startup/logger.js";
+import router from "./startup/routes.js";
+
+router(app);
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
