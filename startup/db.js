41c176c7f439c3ff13e6fed2ae8dbb0e5e93ba0e
Michael B <michaelbarr24@hotmail.com>
2022-06-10 07:49:30 +0200

    Add local DB and Config

--- startup/db.js
+++ startup/db.js
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
 import logger from "./logger.js";
+import config from "config";
 
 export default function () {
-  mongoose.connect("mongodb://127.0.0.1:27017").then(() => {
-    logger.info("Connected to MongoDb");
+  mongoose.connect(config.get("db")).then(() => {
+    logger.info(`Connected to ${config.get("db")}`);
   });
 }
